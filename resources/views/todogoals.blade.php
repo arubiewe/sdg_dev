90ae944e95c38b2efcd17961b387b27a4fe011f6
arubiewe <arubiewe@gmail.com>
2019-01-14 00:19:34 +0100

    news commits

--- resources/views/todogoals.blade.php
+++ resources/views/todogoals.blade.php
@@ -9,6 +9,15 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
     <title>Goals</title>
+  
+
+<!-- Include Bootstrap Datepicker -->
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker.min.css" />
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker3.min.css" />
+
+
+<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
   </head>
   <body>
     
@@ -27,21 +36,21 @@
  <form>
   <div class="form-row">
     <div class="form-group col-md-6">
-      <label for="inputEmail4">Name</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+      <label for="name">Name</label>
+      <input type="text" class="form-control" id="name" placeholder="Name">
     </div>
+    
+    
     <div class="form-group col-md-2">
-      <label for="inputState">Due Date:</label>
-      <select id="inputState"  class="form-control">
-        <option selected>Choose...</option>
-        <option>...</option>
-      </select>
+      <label for="date">Due Date:</label>
+      <input type="date" class="form-control" id="name" placeholder="Name">
     </div>
   </div>
+
   
   <div class="form-row">
     <div class="form-group col-md-4">
-      <label for="inputState">Core Value:</label>
+      <label for="corevalues">Core Value:</label>
       <select id="inputState" class="form-control">
         <option selected></option>
         <option>...</option>
@@ -92,17 +101,46 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 
-    <script type="text/javascript">
-    	
-    	 $( ".date" ).datepicker({
-container: '.datepicker_wrapper'
-  format: "dd-mm-yyyy",
-  autoclose: true,
-  todayHighlight: true
+    <script>
+$(document).ready(function() {
+    $('#datePicker')
+        .datepicker({
+            format: 'mm/dd/yyyy'
+        })
+        .on('changeDate', function(e) {
+            // Revalidate the date field
+            $('#eventForm').formValidation('revalidateField', 'date');
+        });
+
+    $('#eventForm').formValidation({
+        framework: 'bootstrap',
+        icon: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            name: {
+                validators: {
+                    notEmpty: {
+                        message: 'The name is required'
+                    }
+                }
+            },
+            date: {
+                validators: {
+                    notEmpty: {
+                        message: 'The date is required'
+                    },
+                    date: {
+                        format: 'MM/DD/YYYY',
+                        message: 'The date is not a valid'
+                    }
+                }
+            }
+        }
+    });
 });
-
-
-
-    </script>
+</script>
   </body>
 </html>
